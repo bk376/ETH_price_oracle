930f165e3ab1f3ac4e688bdbf2f8ddd0d882113c
bk376 <benardkipcoech@gmail.com>
2021-06-10 23:50:58 -0400

    caller - keep track of ETH price requests

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    mapping(uint256 => bool) myRequests;
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
 
     function setOracleInstanceAddress(address _oracleInstanceAddress)
         public
@@ -16,5 +18,11 @@ contract CallerContract is Ownable {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
         emit newOracleAddressEvent(oracleAddress);
+    }
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
     }
 }
